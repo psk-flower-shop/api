ede233eb06ca83b74baa49f61268b30ca0f66084
tadas <tadas@DESKTOP-E3IRUGR>
2022-05-06 13:32:53 +0300

    Added controllers login and reg, login reg services

---
+++ FlowerApi/Controllers/RegisterController.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using FlowerApi.Entities;
+using FlowerApi.Services;
+using FlowerApi.Services.Interfaces;
+
+namespace FlowerApi.Controllers
+{
+    [ApiController]
+    [Route("api/register")] 
+    public class RegisterUserController : ControllerBase
+    {
+        private readonly IRegisterService _registerService;
+        private readonly ILoginService _loginService;
+
+        public RegisterUserController(IRegisterService registerService, ILoginService loginService)
+        {
+            _loginService = loginService;
+            _registerService = registerService;
+        }
+        
+        [HttpPost]
+        [Route("add")]
+        public ActionResult<IEnumerable<User>> RegisterUser(User user)
+        {
+            try
+            {
+                if (user == null)
+                {
+                    throw new Exception();
+                }
+                _registerService.RegisterUser(user);
+                return Ok(_loginService.GetUsers());
+            }
+            catch(DuplicateNameException)
+            {
+                return Problem("User email already in use!");
+            }
+            catch (Exception)
+            {
+                return Problem("Registration failed");
+            }
+        }
+    }
+}

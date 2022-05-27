45001bd4cde652d4dc2d07b990b3991b3f545ebe
Rytis <lisauskas.rytis@gmail.com>
2022-05-27 03:21:01 +0300

    mapper

---
+++ FlowerApi/Data/MapperProfile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using AutoMapper;
+using FlowerApi.DTO;
+using FlowerApi.Entities;
+
+namespace FlowerApi.Data
+{
+	public class MapperProfile : Profile
+	{
+		public MapperProfile()
+		{
+			CreateMap<User, UserDTO>();
+			CreateMap<UserDTO, User>();
+			CreateMap<CartDTO, Cart>();
+			CreateMap<Cart, CartDTO>();
+			CreateMap<Product, ProductDTO>();
+			CreateMap<ProductDTO, Product>();
+			CreateMap<Category, CategoryDTO>();
+			CreateMap<CategoryDTO, Category>();
+
+		}
+	}
+}
+

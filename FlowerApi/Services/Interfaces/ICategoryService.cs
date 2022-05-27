e815fbaa0db0f3989c42965e7b76c274c0dc3f6c
Andraitis Vilius <vilius.andraitis@mif.stud.vu.lt>
2022-05-27 03:16:55 +0300

    Adding find and findById for categories

---
+++ FlowerApi/Services/Interfaces/ICategoryService.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using FlowerApi.Entities;
+
+namespace FlowerApi.Services.Interfaces
+{
+	public interface ICategoryService
+	{
+		IEnumerable<Category> GetCategories();
+		Category GetCategoryById(Guid id);
+		Task<Category> CreateCategory();
+		Task<Category> DeleteCategory();
+		Task<Category> UpdateCategory();
+	}
+}
+
